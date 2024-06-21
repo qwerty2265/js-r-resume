a07ba65dd5ccdca24fad3dc6c7a1d13605389cb5
Maks <maks_mun@bk.ru>
2024-06-21 12:11:50 +0500

    Create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./page/Home"
+import "./App.css";
+
 	function App() {
 
 	return (
-		<>
-		</>
+		<Routes>
+			<Route path="/" element={<Home />} />
+		</Routes>
 	)
 }
 
